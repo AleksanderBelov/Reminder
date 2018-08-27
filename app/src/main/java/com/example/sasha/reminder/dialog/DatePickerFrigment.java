621d156e2ac22990f76aa227aba479785ef2e3e9
AleksanderBelov <alwh2004@gmail.com>
2018-08-27 17:36:19 +0300

    tree day

---
+++ app/src/main/java/com/example/sasha/reminder/dialog/DatePickerFrigment.java
@@ -0,0 +1,12 @@
+package com.example.sasha.reminder.dialog;
+
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
+import android.widget.DatePicker;
+
+public class DatePickerFrigment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+    }
+}
